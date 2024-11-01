a5b9b2d80b4472482ba4a3058f6aa3b4fc11a673
TheSalt_ <joon_sa@naver.com>
2024-11-01 22:52:26 +0900

    cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const baseURL = "https://solved.ac/api/v3";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/users", async (req, res) => {
